ecf85af20d35cf0c7d7d9867b0b8b387907df30d
Deniev <deniev@iMac-Deniev.local>
2022-05-30 19:25:48 +0300

    Registry class updated

--- app/Registry.php
+++ app/Registry.php
@@ -12,17 +12,21 @@ class Registry
 
     public function __construct()
     {
+        /*/ Путь к файлу log.txt /*/
         $this->log = $GLOBALS['path']['dev'] . $GLOBALS['path']['app'] . _DS_ . 'log.txt';
-        $this->date = (new \DateTime('now'))->format('[H:i | d M Y]');
 
+        /*/ Форматирование текущей даты /*/
+        $this->date = (new \DateTime('now'))->format('[H:i | d M Y]');
     }
 
     public function setException($exception): static
     {
+        /*/ Присвоение значений свойствам если параметр $exception класса Exception /*/
         if ($exception instanceof \Exception) {
             $this->message = $exception->getMessage();
             $this->filename = $exception->getFile();
             $this->line = $exception->getLine();
+        /*/ Присвоение значений свойствам если тип параметра $exception 'array' /*/
         } else {
             $this->message = $exception['message'];
             $this->filename = $exception['filename'];
@@ -34,28 +38,33 @@ class Registry
 
     public function getException($type = null): array|string
     {
-        if ($type != 'array') {
-            $date = '<span style="color: #ce4040">' . $this->date . '</span> ';
-
-            return $date . $this->message . ' <b>' . $this->filename . '</b> <small>(line ' . $this->line . ')</small><br>';
-        } else {
+        /*/ Если запрошен тип исключения 'array' выдать исключение в виде массива /*/
+        if ($type == 'array') {
             return [
                 'date' => $this->date,
                 'message' => $this->message,
                 'filename' => $this->filename,
                 'line' => $this->line
             ];
+        /*/ В противном случае выдать исклюение в форматированном виде /*/
+        } else {
+            $date = '<span style="color: #ce4040">' . $this->date . '</span> ';
+
+            return $date . $this->message . ' <b>' . $this->filename . '</b> <small>(line ' . $this->line . ')</small><br>';
         }
     }
 
     public function writeLog()
     {
+        /*/ Создание новой записи текущего исключения /*/
         $entry = PHP_EOL . $this->date .' '. $this->message .' '. $this->filename .' (line '. $this->line .')';
 
+        /*/ Удаление записи из log.txt если такая запись уже существует /*/
         $contents = file_get_contents($this->log);
         $contents = str_replace($entry, '', $contents);
-
         file_put_contents($this->log, $contents);
+
+        /*/ Добавление новой записи в log.txt /*/
         file_put_contents($this->log, $entry, FILE_APPEND | LOCK_EX);
     }
 }

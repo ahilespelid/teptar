ea4ce86a587128fd8f60797c16bdaa861da03c5d
Ahi Les <ahilespelid@yandex.ru>
2022-05-30 20:18:35 +0300

    -

--- app/Registr.php
+++ app/Registr.php
@@ -1,89 +1,52 @@
-<?php
+<?php namespace App;
 
-<<<<<<< HEAD:app/Registr.php
 class Registr{
-=======
-namespace App;
-
-class Registry
-{
->>>>>>> f6fd1e3818409331b7e47c2e445de17edc3b336a:app/Registry.php
     public  $log,
-            $date,
-            $message,
-            $filename,
-            $line;
+                $date,
+                $message,
+                $filename,
+                $line;
 
-    public function __construct()
-    {
-        /*/ Путь к файлу log.txt /*/
-        $this->log = $GLOBALS['path']['dev'] . $GLOBALS['path']['app'] . _DS_ . 'log.txt';
+    public function __construct(){
+        $this->log = $GLOBALS['path']['dev'] . $GLOBALS['path']['cor'] . _DS_ . 'log.txt';                                                                                                          /*/ Путь к файлу log.txt /*/
+        $this->date = (new \DateTime('now'))->format('[H:i | d M Y]');                                                                                                                                     /*/ Форматирование текущей даты /*/
 
-        /*/ Форматирование текущей даты /*/
-        $this->date = (new \DateTime('now'))->format('[H:i | d M Y]');
     }
 
-<<<<<<< HEAD:app/Registr.php
-    public function setException($exception){
-=======
-    public function setException($exception): static
-    {
-        /*/ Присвоение значений свойствам если параметр $exception класса Exception /*/
->>>>>>> f6fd1e3818409331b7e47c2e445de17edc3b336a:app/Registry.php
+    public function setException($exception){                                                                                                                                                                          /*/ Присвоение значений свойствам если параметр $exception класса Exception /*/
         if ($exception instanceof \Exception) {
             $this->message = $exception->getMessage();
             $this->filename = $exception->getFile();
             $this->line = $exception->getLine();
-        /*/ Присвоение значений свойствам если тип параметра $exception 'array' /*/
-        } else {
+        } else {                                                                                                                                                                                                                                /*/ Присвоение значений свойствам если тип параметра $exception 'array' /*/
             $this->message = $exception['message'];
             $this->filename = $exception['filename'];
             $this->line = $exception['line'];
         }return $this;
     }
 
-<<<<<<< HEAD:app/Registr.php
     public function getException($type = null){
-        if ($type != 'array') {
+        if ($type != 'array') {                                                                                                                                                                                                            /*/ Если запрошен тип исключения 'array' выдать исключение в виде массива /*/
             $date = '<span style="color: #ce4040">' . $this->date . '</span> ';
 
             return $date . $this->message . ' <b>' . $this->filename . '</b> <small>(line ' . $this->line . ')</small><br>';
         } else {
-=======
-    public function getException($type = null): array|string
-    {
-        /*/ Если запрошен тип исключения 'array' выдать исключение в виде массива /*/
-        if ($type == 'array') {
->>>>>>> f6fd1e3818409331b7e47c2e445de17edc3b336a:app/Registry.php
-            return [
+            return [                                                                                                                                                                                                                          /*/ В противном случае выдать исклюение в форматированном виде /*/
                 'date' => $this->date,
                 'message' => $this->message,
                 'filename' => $this->filename,
                 'line' => $this->line
             ];
-        /*/ В противном случае выдать исклюение в форматированном виде /*/
-        } else {
-            $date = '<span style="color: #ce4040">' . $this->date . '</span> ';
-
-            return $date . $this->message . ' <b>' . $this->filename . '</b> <small>(line ' . $this->line . ')</small><br>';
         }
     }
 
-<<<<<<< HEAD:app/Registr.php
-    public function writeLog(){
-=======
-    public function writeLog()
-    {
-        /*/ Создание новой записи текущего исключения /*/
->>>>>>> f6fd1e3818409331b7e47c2e445de17edc3b336a:app/Registry.php
+    public function writeLog(){                                                                                                                                                                                                     /*/ Создание новой записи текущего исключения /*/
         $entry = PHP_EOL . $this->date .' '. $this->message .' '. $this->filename .' (line '. $this->line .')';
 
-        /*/ Удаление записи из log.txt если такая запись уже существует /*/
         $contents = file_get_contents($this->log);
-        $contents = str_replace($entry, '', $contents);
-        file_put_contents($this->log, $contents);
+        $contents = str_replace($entry, '', $contents);                                                                                                                                                                   /*/ Удаление записи из log.txt если такая запись уже существует /*/
 
-        /*/ Добавление новой записи в log.txt /*/
-        file_put_contents($this->log, $entry, FILE_APPEND | LOCK_EX);
+        file_put_contents($this->log, $contents);
+        file_put_contents($this->log, $entry, FILE_APPEND | LOCK_EX);                                                                                                                                      /*/ Добавление новой записи в log.txt /*/
     }
 }

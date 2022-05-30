3073e74d86eae7e310c6b4a8f5712c5455353a51
Deniev <deniev@iMac-Deniev.local>
2022-05-30 18:50:30 +0300

    Registry class updated

--- app/Registry.php
+++ app/Registry.php
@@ -4,40 +4,58 @@ namespace App;
 
 class Registry
 {
-    public string $file;
-    public array $object = [
-        'type' => 'Fatal Error',
-        'date' => '24 окт. 2022',
-        'message' => 'Undefined constant "variable"',
-        'filename' => 'www/public/index.php',
-        'line' => '17',
-
-    ];
+    public  $log,
+            $date,
+            $message,
+            $filename,
+            $line;
 
     public function __construct()
     {
-        $this->file = $GLOBALS['path']['dev'] . $GLOBALS['path']['app'] . _DS_ . 'log.txt';
+        $this->log = $GLOBALS['path']['dev'] . $GLOBALS['path']['app'] . _DS_ . 'log.txt';
+        $this->date = (new \DateTime('now'))->format('[H:i | d M Y]');
+
     }
 
-    public function setException()
+    public function setException($exception): static
     {
-        $object = $this->object;
-
-        $entry = null;
-
-        foreach ($object as $key => $value) {
-            if ($key == 'type') {
-                $entry .= $value;
-            } elseif ($key == 'date') {
-                $entry .= $value;
-            }
+        if ($exception instanceof \Exception) {
+            $this->message = $exception->getMessage();
+            $this->filename = $exception->getFile();
+            $this->line = $exception->getLine();
+        } else {
+            $this->message = $exception['message'];
+            $this->filename = $exception['filename'];
+            $this->line = $exception['line'];
         }
 
-        var_dump($entry);
+        return $this;
+    }
 
+    public function getException($type = null): array|string
+    {
+        if ($type != 'array') {
+            $date = '<span style="color: #ce4040">' . $this->date . '</span> ';
 
-//        $entry = '['. $object['date'] . '] [' . $object['type'] . '] ' . $object['message'] . ' | ' . $object['filename'] . ' (line ' . $object['line'] . ')';
-//
-//        file_put_contents($this->file, $entry, FILE_APPEND | LOCK_EX);
+            return $date . $this->message . ' <b>' . $this->filename . '</b> <small>(line ' . $this->line . ')</small><br>';
+        } else {
+            return [
+                'date' => $this->date,
+                'message' => $this->message,
+                'filename' => $this->filename,
+                'line' => $this->line
+            ];
+        }
+    }
+
+    public function writeLog()
+    {
+        $entry = PHP_EOL . $this->date .' '. $this->message .' '. $this->filename .' (line '. $this->line .')';
+
+        $contents = file_get_contents($this->log);
+        $contents = str_replace($entry, '', $contents);
+
+        file_put_contents($this->log, $contents);
+        file_put_contents($this->log, $entry, FILE_APPEND | LOCK_EX);
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,11 +4,43 @@ use App\Registry;
 /*/ Ядро /*/
 require_once('..'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php');
 //$route = new App\Route; $route->run();
-//$test = sdffd;
-
-/*throw new ErrorException('Test error', '<?php phpinfo() ?>','','index.php','17','');*/
-
-//$esff = fds;
 
 $registry = new Registry();
-$registry->setException();
+
+$object = [
+    'message' => 'Undefined constant "variable"',
+    'filename' => 'www/public/index.php',
+    'line' => '17',
+];
+
+try {
+    throw new Exception();
+} catch (Exception $exception) {
+    $registry->setException($object);
+    echo $registry->getException();
+}
+
+try {
+    throw new Exception('The value has to be 1 or lower');
+} catch (Exception $exception) {
+    $registry->setException($exception);
+    echo $registry->getException();
+}
+
+try {
+    throw new Exception('The value has to be 1 or lower');
+} catch (Exception $exception) {
+    echo '<br>';
+    $registry->setException($exception);
+    pa($registry->getException('array'));
+    $registry->writeLog();
+}
+
+try {
+    throw new Exception('The value has to be 1 or lower');
+} catch (Exception $exception) {
+    echo '<br>';
+    $registry->setException($object);
+    pa($registry->getException('array'));
+    $registry->writeLog();
+}

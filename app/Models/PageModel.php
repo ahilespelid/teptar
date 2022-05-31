a0a6c718576fa66fe23ac5ea7197838caed5d1bd
Ahi Les <ahilespelid@gmail.com>
2022-05-31 16:10:24 +0300

    ok

---
+++ app/Models/PageModel.php
@@ -0,0 +1,13 @@
+<?php namespace App\Models; use App;
+
+class PageModel extends App\Model{
+    public $resID, $resRange;
+
+    public function getById($id){
+        return $this->PDO->query("SELECT * FROM page WHERE id=$id");
+    }
+
+    public function getByRange($from, $to){
+        return $this->PDO->query("SELECT * FROM page WHERE id>=$from AND id<=$to");
+    }
+}

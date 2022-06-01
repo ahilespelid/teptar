6baeffab74bff3567669a039908bbc6fb64bf7e9
Deniev <deniev@iMac-Deniev.local>
2022-06-01 20:20:48 +0300

    Comparison block added

---
+++ public/maga/blocks/comparison/comparison.js
@@ -0,0 +1,18 @@
+// Создание цвета для индикатора эффективности и добавление элементов верстки
+document.querySelectorAll('.districts-score-table').forEach((table) => {
+    // Создание цвета из диапазона двух цветов для индикатора эффективности в зависимовсти от процента
+    table.querySelectorAll('.table-progress-bar').forEach((bar) => {
+        bar.querySelector('.table-progress-bar-gradient').innerHTML = bar.style.width;
+        bar.style.backgroundColor = hslColor(bar.style.width.replace('%',''),0,120);
+    })
+
+    // Добавление элементов 'separator' для улучшения верстки
+    table.querySelector('thead tr').insertAdjacentHTML('afterbegin','<th class="separator"></th>');
+    table.querySelector('thead tr').insertAdjacentHTML('beforeend','<th class="separator"></th>');
+    table.querySelectorAll('tbody tr').forEach((row) => {
+        row.insertAdjacentHTML('afterbegin','<td class="separator"></td>');
+    })
+    table.querySelectorAll('tbody tr').forEach((row) => {
+        row.insertAdjacentHTML('beforeend','<td class="separator"></td>');
+    })
+});
--- public/maga/index.php
+++ public/maga/index.php
@@ -1,124 +1,36 @@
 <!DOCTYPE html>
 <html lang="ru">
     <head>
-        <title>Шаблон</title>
-        <meta http-equiv="Expires" content="Fri, Jan 01 1900 00:00:00 GMT">
-        <meta http-equiv="Pragma" content="no-cache">
-        <meta http-equiv="Cache-Control" content="no-cache">
+        <title>Teptar</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <meta http-equiv="Lang" content="en">
-        <meta name="author" content="">
-        <meta http-equiv="Reply-to" content="@.com">
-        <meta name="generator" content="PhpED 8.0">
-        <meta name="description" content="">
-        <meta name="keywords" content="">
-        <meta name="creation-date" content="09/06/2012">
-        <meta name="revisit-after" content="15 days">
-        <link rel="stylesheet" type="text/css" href="blocks/styles.css">
+        <meta name="description" content="Шаблон индексной страницы">
+        <meta name="keywords" content="Шаблон индексной страницы">
+
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="blocks/styles.css">
+        <link rel="stylesheet" type="text/css" href="blocks/browsers.css">
+        <link rel="stylesheet" type="text/css" href="blocks/fonts.css">
     </head>
 
     <body>
+
+        <?php include 'blocks/navbar/navbar.html' ?>
+
         <div class="container">
-            <?php include 'blocks/start/start.html';?>
-            <?php include 'blocks/rating/rating.html';?>
-            <?php include 'blocks/statistics/statistics.html';?>
-            <?php include 'blocks/comparison/comparison.html';?>
 
-            <div class="actions">
-
-                <div class="actions__reports">
-
-                    <div class="actions__title">
-                        <h3>Последние отчеты</h3>
-                    </div>
-
-                    <div class="actions__info">
-
-                        <div class="actions__reports-info-item">
-                            <div class="actions__reports-info-item-title">
-                                Отчет 2022
-                                <div class="actions__reports-date"><i class="icon-document"></i> Отчет завершен 21.05.2022</div>
-                            </div>
-                            <div class="actions__reports-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed id semper risus in hendrerit gravida rutrum. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.</div>
-                            <div class="actions__reports-rating">
-                                Оценка:
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                            </div>
-                        </div>
-
-                        <div class="actions__reports-info-item">
-                            <div class="actions__reports-info-item-title">
-                                Отчет 2022
-                                <div class="actions__reports-date"><i class="icon-document"></i> Отчет завершен 21.05.2022</div>
-                            </div>
-                            <div class="actions__reports-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed id semper risus in hendrerit gravida rutrum. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.</div>
-                            <div class="actions__reports-rating">
-                                Оценка:
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                            </div>
-                        </div>
-
-                        <div class="actions__reports-info-item">
-                            <div class="actions__reports-info-item-title">
-                                Отчет 2022
-                                <div class="actions__reports-date"><i class="icon-document"></i> Отчет завершен 21.05.2022</div>
-                            </div>
-                            <div class="actions__reports-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed id semper risus in hendrerit gravida rutrum. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.</div>
-                            <div class="actions__reports-rating">
-                                Оценка:
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                            </div>
-                        </div>
-
-                        <div class="actions__reports-info-item">
-                            <div class="actions__reports-info-item-title">
-                                Отчет 2022
-                                <div class="actions__reports-date"><i class="icon-document"></i> Отчет завершен 21.05.2022</div>
-                            </div>
-                            <div class="actions__reports-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed id semper risus in hendrerit gravida rutrum. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.</div>
-                            <div class="actions__reports-rating">
-                                Оценка:
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                                <i class="icon-check"></i>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class="actions__activity">
-
-                    <div class="actions__title">
-                        <h3>Активность</h3>
-                    </div>
-
-                    <div class="actions__info"></div>
-
-                </div>
-
+            <div class="main">
+                <?php include 'blocks/start/start.html';?>
+                <?php include 'blocks/districts/districts.html';?>
+                <?php include 'blocks/rating/rating.html';?>
+                <?php include 'blocks/comparison/comparison.html';?>
+                <?php include 'blocks/actions/actions.html';?>
             </div>
 
         </div>
+
     </body>
+
+    <script type="text/javascript" src="blocks/scripts.js"></script>
+    <script type="text/javascript" src="blocks/comparison/comparison.js"></script>
 
 </html>

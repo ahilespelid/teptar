454ba889560a9bf8f214792c39272b7204dd5cb0
Ahi Les <ahilespelid@yandex.ru>
2022-06-08 23:02:53 +0300

    AjaxController create

---
+++ app/Controllers/AjaxController.php
@@ -0,0 +1,21 @@
+<?php 
+namespace App\Controllers ;
+use App\Models;
+
+class AjaxController{
+    public          $model;
+
+    public function __construct() {
+        $this->model = new \App\Models\AjaxModel;
+    }
+/*/ -------------------------------------------------------------- Аякс для главной временный -------------------------------------------------------------- /*/   
+    public function getMarkData($q){
+        header("Content-type: application/json; charset=utf-8");
+        $db =  $this->model;
+        $return = $db->getWhere('indexes',$q);
+        $json = json_encode($return, JSON_FORCE_OBJECT);
+        
+        echo $json; exit();
+        //pa($return);
+        }  
+}
--- app/Controllers/ExelController.php
+++ app/Controllers/ExelController.php
@@ -38,10 +38,10 @@ class ExelController{
             $data[$sheetName] = $ar;
         } 
         
-/*/     \pa($data);   /*/ 
+        $dataLoad = $data; 
+         /*/   /*/ 
  
- 
-        
+/*/         
  $sql = " "; $l = 1;      
  foreach($data as $k => $v){
      for($i=0, $c = count($v); $i < $c; $i++) {
@@ -49,131 +49,29 @@ class ExelController{
  "VALUES ('".$k."', '".$v[$i][0]."', '".$v[$i][1]."',  '".$v[$i][2]."',  '".$v[$i][3]."',   '".$v[$i][4]."',   '".$v[$i][5]."',  '".$v[$i][6]."',  '".$v[$i][7]."',  '".$v[$i][8]."',   '".$v[$i][9]."',   '".$v[$i][10]."',   '".$v[$i][11]."', '".$v[$i][12]."',  '".$v[$i][13]."');  <br>";
          $l++;}                     
      }
+     echo  $sql;  /*/     
+
+ foreach($dataLoad as $k=>$v){
      
-     echo  $sql;    
-       //
-       
-       
-/*/        
-        if(!empty($_FILES)){          
-            $uploadfile = $uploaddir.basename($_FILES['file']['name']); 
-            move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
-            echo $uploadfile;  
-        }else{
-echo '<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Drag and Drop exel</title>  
-        <style type="text/css">
-body {font: 12px Arial, sans-serif;}
-#dropZone {color: #555; font-size: 18px; text-align: center; width: 400px; padding: 50px 0; margin: 50px auto; background: #eee; border: 1px solid #ccc; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px;}
-#dropZone.hover {background: #ddd;border-color: #aaa;}
-#dropZone.error {background: #faa; border-color: #f00;}
-#dropZone.drop {background: #afa;border-color: #0f0;} 
-        </style>
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-  <script type="text/javascript">
- $(document).ready(function() {   
-    var dropZone = $("#dropZone"),                                                        
-        maxFileSize = 50000000; // максимальный размер фалйа - 50 мб.
-    
-    // Проверка поддержки браузером
-    if (typeof(window.FileReader) == "undefined") {
-        dropZone.text("Не поддерживается браузером!");
-        dropZone.addClass("error");
-    }
-    
-    // Добавляем класс hover при наведении
-    dropZone[0].ondragover = function() {
-        dropZone.addClass("hover");
-        return false;
-    };
-    
-    // Убираем класс hover
-    dropZone[0].ondragleave = function() {
-        dropZone.removeClass("hover");
-        return false;
-    };
-    
-    // Обрабатываем событие Drop
-    dropZone[0].ondrop = function(event) {
-        event.preventDefault();
-        dropZone.removeClass("hover");
-        dropZone.addClass("drop");
-        
-        var file = event.dataTransfer.files[0];
-        
-        // Проверяем размер файла
-        if (file.size > maxFileSize) {
-            dropZone.text("Файл слишком большой!");
-            dropZone.addClass("error");
-            return false;
-        }
-
-        // Проверяем тип
-        var fileName = $(file)[0].name;
-        var extension = fileName.split(".").pop().toLowerCase();
-        if (~$.inArray(extension, ["xlsm", "xls", "xlsx"])) {
-            console.log("file "+fileName+" load");
-        } else {
-            dropZone.text("Файл не exel!");
-            dropZone.addClass("error");
-            return false;
-        }
-        
-        // Создаем запрос
-        var xhr = new XMLHttpRequest();
-        xhr.upload.addEventListener("progress", uploadProgress, false);
-        xhr.onreadystatechange = stateChange;
-        xhr.open("POST", "/exel");
-        xhr.setRequestHeader("X-FILE-NAME", file.name);
-
-        
-        xhr.send(file);
-    };
-    
-    // Показываем процент загрузки
-    function uploadProgress(event) {
-        var percent = parseInt(event.loaded / event.total * 100);
-        dropZone.text("Загрузка: " + percent + "%");
-    }
-    
-    // Пост обрабочик
-    function stateChange(event) {
-        if (event.target.readyState == 4) {
-            if (event.target.status == 200) {
-                console.log(event.target.responseText);
-                dropZone.text("Загрузка успешно завершена!");
-            } else {
-                dropZone.text("Произошла ошибка!");
-                dropZone.addClass("error");
-            }
-        }
-    }
-    
-});   
-  </script>
-</head>
-
-<body>
-    
-    <form action="/exel">
-      <div id="dropZone">
-        Для загрузки, перетащите файл сюда.
-      </div>
-    </form>
-  
-</body>
-</html>
-'; 
-        }
- /*/       
+     for($i=0, $c = count($v); $i < $c; $i++) {
+         
+         
+          for($l=1, $s = count($v[$i]); $l < $s; $l++) {
+               
+              $v[$i][$l] = (empty($v[$i][$l])) ? 0 : $v[$i][$l];
+              //echo $v[$i][$l].'<br>';
+              $stroka[$v[$i][0]][] = $v[$i][$l];
+          }//echo '<br>';
+          
+          
+     
+     } $dataRet[$k] = $stroka; unset($stroka);
+ }  
+     
+pa($dataRet);         
     }
  /*/ -------------------------------------------------------------- Загрузка exel -------------------------------------------------------------- /*/     
     public function load(){
-        $path = realpath('./').DIRECTORY_SEPARATOR.'404.php';
-       if(file_exists($path)){include($path);}
         exit();       
     }    
 }

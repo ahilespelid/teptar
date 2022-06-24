4ef9e2a04e9d54b775bfce6d055babd36a8e03fa
Ahi Les <ahilespelid@yandex.ru>
2022-06-24 22:04:38 +0300

    new

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Controllers;
+
+class HomeController extends AbstractController {
+    public $user;
+    public function __construct(){
+        $this->user = new UserController;
+    }
+
+    /**
+     * Главная страница
+     * @throws Exception
+     */
+    public function index() {
+        if(isset($_GET['logout'])) {
+            $this->user->out(); exit();
+        }
+
+        if ($this->user->isToken()) {
+            $this->render('/home/login.php');
+        } else {
+            $this->render('/home/login.php');
+        }
+    }
+
+    /**
+     * Страница ошибок
+     * @throws Exception
+     */
+    public function error() {
+        $error = '404';
+        $title = 'Страница не найдена';
+        $message = 'Страницу которую вы искали не существует, пожалуйста выберите другую либо вернитесь на главную.';
+
+        $this->render('/home/error.php', [
+            'error' => $error,
+            'title' => $title,
+            'message' => $message
+        ]);
+    }
+}

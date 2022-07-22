ed3ceda85170b0c90eb6361dba05118aa6a844d2
Deniev <deniev@iMac-Deniev.local>
2022-07-22 18:17:40 +0300

    Centers page added & links to call center

--- core/rules.php
+++ core/rules.php
@@ -1,15 +1,15 @@
 <?php return [
     'authorized' => ['index', 'framework', 'notifications'],
 
-    'region_boss' => ['district', 'districtReports', 'leader', 'staff', 'ratingByMark', 'profile', 'support', 'callCenter', 'uinData'],
+    'region_boss' => ['district', 'districtReports', 'leader', 'staff', 'ratingByMark', 'profile', 'support', 'callCenter', 'uinData', 'center'],
 
-    'ministry_boss' => ['reports', 'report', 'districts', 'districtNotifications', 'profile', 'support', 'callCenter', 'uinData'],
+    'ministry_boss' => ['reports', 'report', 'districts', 'districtNotifications', 'profile', 'support', 'callCenter', 'uinData', 'center'],
 
-    'district_boss' => ['districts', 'reports', 'report', 'report/new', 'profile', 'support', 'callCenter', 'uinData'],
+    'district_boss' => ['districts', 'reports', 'report', 'report/new', 'profile', 'support', 'callCenter', 'uinData', 'center'],
 
-    'district_staff' => ['reports', 'report', 'profile', 'support', 'callCenter', 'uinData'],
+    'district_staff' => ['reports', 'report', 'profile', 'support', 'callCenter', 'uinData', 'center'],
 
     'guest' => ['index', 'login'],
 
-    'admin_admin' => ['calculate','district', 'districtReports', 'leader', 'staff', 'profile', 'support', 'callCenter', 'uinData']
+    'admin_admin' => ['calculate','district', 'districtReports', 'leader', 'staff', 'profile', 'support', 'callCenter', 'uinData', 'center']
 ];

21a151a4f20981612a2b38f869524140e7b72914
Ahi Les <ahilespelid@yandex.ru>
2022-07-29 20:51:49 +0300

    code for mag3

--- app/Models/IndexModel.php
+++ app/Models/IndexModel.php
@@ -9,26 +9,16 @@ class IndexModel extends \App\Data{
         (object)$this->pdo = $this->connPDO();
     }
     public function reportActions($id_repotr){if(!is_numeric($id_repotr)){return false;}
-        $sql = "SELECT * FROM `index` WHERE `id_report` = '$id_repotr' ORDER BY id_mark+0 ASC, date DESC";
-        $return = $this->getQuery($sql); $r = [];
+        $sql = "SELECT * FROM `index` WHERE `id_report`= '$id_repotr' ORDER BY id_mark+0 ASC, date DESC";
+        $indexes = $this->getQuery($sql); $r = [];
         
-        $indexes = $this->getQuery($sql); $r = [];       
         foreach($indexes as $key => $val){foreach($val as $k => $v){if('id_mark' == $k){$r[$v] []= $val;}}}
         
         if(is_array($r) && !empty($r)){$return = [];
-            foreach($r as $key => $index){foreach($index as $k => $v){$table =$this->getTableFromIdString($k);
-                $return[$key][$k] =(!empty($table)) ? $this->getId($table,$v) : $return[$key][$k] = $v;
-        }}}else{return false;}//*/ $return; //*/
+            foreach($r as $mark => $indexesAction){foreach($indexesAction as $num => $indexAction){foreach($indexAction as $k => $v){$table =$this->getTableFromIdString($k);
+                $return[$mark][$num][$k] =(!empty($table)) ? $this->getId($table,$v) : $return[$mark][$num][$k] = $v;
+        }}}}else{return false;}//*/ $return; //*/
         
         return (is_array($return) && !empty($return)) ? $return : null;
     }
-
-    public function reportActivities($reportId) {
-        $sql = 'SELECT *
-                FROM `' . $this->table . '`
-                WHERE id_report = ' . $reportId . '
-                ORDER BY id_mark+0 ASC, `date` DESC';
-
-        return $this->customSQL($sql);
-    }
-}
+} 
